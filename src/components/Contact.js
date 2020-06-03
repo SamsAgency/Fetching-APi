7eb2c35b37e1312bbf6cdc8c361ca8ee9bafa66b
SamsAgency <samstore637@gmail.com>
2020-06-03 13:01:15 +0300

    my first axios react app

---
+++ src/components/Contact.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Contact = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('./about')
+    // }, 3000)
+
+  return (
+    <div className="container">
+      <h3 className="center"> Contact </h3>
+      <p>
+        
+        Eu cupidatat velit incididunt qui veniam eiusmod. Aute non do tempor
+        laboris consequat dolor. Ipsum amet culpa cillum nulla aliquip fugiat
+        veniam minim aliquip.
+      
+        Voluptate incididunt excepteur aliquip sunt consectetur. Et laboris
+        pariatur laborum duis ad esse. Pariatur consectetur enim dolor cupidatat
+        incididunt est voluptate mollit non nisi eu consectetur anim laboris.
+        Labore veniam elit est ipsum nulla sunt. Enim minim eu reprehenderit
+        fugiat minim duis excepteur amet id do culpa deserunt.
+      
+        Nisi pariatur aliqua veniam fugiat. Est cillum cupidatat fugiat commodo.
+        Laboris laboris adipisicing in aliquip sunt eiusmod pariatur ex
+        reprehenderit duis non. Laborum aute culpa labore culpa.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
